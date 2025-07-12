c616d56b2a800ccbadf1c331d7ba612f92c72392
Aurelien <aureplusyt@gmail.com>
2025-07-12 09:29:44 +0200

    getOrdersFromEventId fix (added the onRequest...)

--- frontend/src/utils/dbFetch/getOrdersFromEventId.ts
+++ frontend/src/utils/dbFetch/getOrdersFromEventId.ts
@@ -4,11 +4,15 @@ import type Order from "../../types/OrderType.ts";
 export default async function getOrdersFromEventId(
   id: string,
   setOrders: (orders: Order[]) => void,
+  onRequestStart: () => void = () => {},
+  onRequestEnd: () => void = () => {},
   onError: () => void = () => {},
   onSuccess: () => void = () => {}
 ) {
+  onRequestStart();
   try {
     const response = await fetch(`${api_url}/api/events/${id}/orders`);
+    onRequestEnd();
     if (!response.ok) {
       onError();
       throw new Error("Network response was not ok");
@@ -17,6 +21,7 @@ export default async function getOrdersFromEventId(
     setOrders(data);
     onSuccess();
   } catch (error) {
+    onRequestEnd();
     onError();
     console.error("Error fetching orders:", error);
   }
