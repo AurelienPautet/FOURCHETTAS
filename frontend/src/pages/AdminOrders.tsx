a628abc19bac5bb03dee30bd6a7109383d0caf85
Aurelien <aureplusyt@gmail.com>
2025-07-12 16:17:44 +0200

    Stats on AdminOrder page

--- frontend/src/pages/AdminOrders.tsx
+++ frontend/src/pages/AdminOrders.tsx
@@ -21,7 +21,7 @@ function AdminOrders() {
   }
 
   const [eventData, setEventData] = useState<Event | null>(null);
-
+  const [CA, setCA] = useState<number>(0);
   const [dishes, setDishes] = useState<Item[]>([]);
   const [sides, setSides] = useState<Item[]>([]);
   const [drinks, setDrinks] = useState<Item[]>([]);
@@ -47,6 +47,25 @@ function AdminOrders() {
     setItemsMap(map);
   }
 
+  function calculateCA() {
+    const total = orders.reduce((acc, order) => {
+      const dish = itemsMap.get(order.dish_id);
+      const side = itemsMap.get(order.side_id);
+      const drink = itemsMap.get(order.drink_id);
+
+      const dishPrice = dish ? dish.price : 0;
+      const sidePrice = side ? side.price : 0;
+      const drinkPrice = drink ? drink.price : 0;
+
+      return acc + Number(dishPrice) + Number(sidePrice) + Number(drinkPrice);
+    }, 0);
+    setCA(total);
+  }
+
+  useEffect(() => {
+    calculateCA();
+  }, [orders, itemsMap]);
+
   useEffect(() => {
     getItemsFromEventId(Number(id), setDishes, setSides, setDrinks);
   }, []);
@@ -59,19 +78,30 @@ function AdminOrders() {
     <div className="flex-grow h-full w-full flex flex-col gap-4 pr-4 pl-4 pb-4  overflow-x-hidden  overflow-y-scroll">
       <NavbarSpacer />
       <div className="flex flex-col items-center gap-4">
-        <h1 className="text-3xl font-bold">
+        <h1 className="text-3xl font-bold text-center">
           Les commandes pour l'événement{" "}
           <span className="font-extrabold">{eventData?.title}</span> du{" "}
           {TextDate(eventData?.date, eventData?.time)}
         </h1>
-        <div className="stats shadow h-full ">
-          <div className="stat">
-            <div className="stat-title">Total Page Views</div>
-            <div className="stat-value">89,400</div>
-            <div className="stat-desc">21% more than last month</div>
+        <StatusEvent event={eventData} />
+        <h1 className="text-2xl font-bold">Les statistiques</h1>
+        <div className="flex flex-row gap-4">
+          <div className="stats shadow h-full w-50 bg-base-200 ">
+            <div className="stat">
+              <div className="stat-title">Nombre de commandes</div>
+              <div className="stat-value">{orders.length}</div>
+              <div className="stat-desc">c'est pas mal</div>
+            </div>
+          </div>
+          <div className="stats shadow h-full w-50 bg-base-200">
+            <div className="stat">
+              <div className="stat-title">Ca estimé</div>
+              <div className="stat-value">{CA} €</div>
+              <div className="stat-desc">la moula</div>
+            </div>
           </div>
         </div>
-        <StatusEvent event={eventData} />
+        <h1 className="text-2xl font-bold">Résumé des commandes</h1>
         <OverviewOrder
           event={eventData}
           dishes={dishes}
