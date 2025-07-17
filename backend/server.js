3f577530ea2e85c3c124fa026b37f1379a843b63
Aurelien <aureplusyt@gmail.com>
2025-07-17 15:52:48 +0200

    new cross-policy headers for imgly Multithreading (not working currently)

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,14 @@ const PORT = process.env.PORT;
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  next();
+});
+
 app.use(helmet());
-app.use(express.json());
+app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
