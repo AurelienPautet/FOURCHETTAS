d9903165be08a6014d2253bd0bade25ad2206004
Aurelien <aureplusyt@gmail.com>
2025-07-21 22:12:12 +0200

    cors options updated

--- backend/index.js
+++ backend/index.js
@@ -30,9 +30,6 @@ const corsOptions = {
     "http://localhost:5173",
     "http://localhost:3000",
   ],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
 };
 
 app.use(cors(corsOptions));
