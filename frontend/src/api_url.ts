c0d00f696bbd12398a99f6efa7acd8adabee4ac3
Aurelien <aureplusyt@gmail.com>
2025-08-31 20:02:30 +0200

    oopsie

--- frontend/src/api_url.ts
+++ frontend/src/api_url.ts
@@ -1,5 +1,5 @@
-const api_url = "http://localhost:3000";
+//const api_url = "http://localhost:3000";
 
-//const api_url = "https://fourchettas.vercel.app";
+const api_url = "https://fourchettas.vercel.app";
 
 export default api_url;
