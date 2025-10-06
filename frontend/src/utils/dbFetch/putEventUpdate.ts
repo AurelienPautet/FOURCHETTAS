e2a999ff1ff77c101d159dc2e5c74f0d0e5ea21c
Aurelien <aureplusyt@gmail.com>
2025-10-06 07:13:17 +0200

    fix error

--- frontend/src/utils/dbFetch/putEventUpdate.ts
+++ frontend/src/utils/dbFetch/putEventUpdate.ts
@@ -2,25 +2,34 @@ import api_url from "../../api_url";
 import type resType from "../../types/ResType";
 
 interface putEventUpdateProps extends resType {
-    id : number,
-    eventData : any,
+  id: number;
+  eventData: any;
 }
 
-export default async function putEventUpdate({id,eventData,onRequestStart = () => {} ,onRequestEnd = () => {} , onSuccess = ()=>{} ,onError = ()=>{}}:putEventUpdateProps) {
-    onRequestStart();
-    fetch(`${api_url}/api/orders`,{
-        method: "PUT",
-        headers: {"Content-Type" : "application/json"},
-        body:
-    }).then((response)=>{
-        onRequestEnd();
-        if(!response.ok){
-            onError()
-            throw new Error("Network response was not ok");
-        }
-        return response.json();
-    }).then((data)=>{
-        console.log("Event modified successfully", data);
-        onSuccess();
+export default async function putEventUpdate({
+  id,
+  eventData,
+  onRequestStart = () => {},
+  onRequestEnd = () => {},
+  onSuccess = () => {},
+  onError = () => {},
+}: putEventUpdateProps) {
+  onRequestStart();
+  fetch(`${api_url}/api/orders`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ id, eventData }),
+  })
+    .then((response) => {
+      onRequestEnd();
+      if (!response.ok) {
+        onError();
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
     })
+    .then((data) => {
+      console.log("Event modified successfully", data);
+      onSuccess();
+    });
 }
