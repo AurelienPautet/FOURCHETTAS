9e035ee999bc891d2351b25862de340f510f87e0
Aurelien <aureplusyt@gmail.com>
2025-10-06 10:44:54 +0200

    fix vercel serverless functions

--- backend/index.js
+++ backend/index.js
@@ -48,6 +48,10 @@ app.get("/api/health", (req, res) => {
   res.json({ status: "OK", message: "Server is running smoothly!" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}
+
+export default app;
